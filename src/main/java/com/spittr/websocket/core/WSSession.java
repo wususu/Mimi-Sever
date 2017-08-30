c121fdb12fc4b7fbbdfda429113cf71890d8eadb
janke <keven_susu@126.com>
2017-08-31 00:44:23 +0800

    websocket user session

---
+++ src/main/java/com/spittr/websocket/core/WSSession.java
@@ -0,0 +1,32 @@
+package com.spittr.websocket.core;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.spittr.user.model.User;
+
+
+public class WSSession {
+	
+	private static final Set<User> USER_SESSION;
+	
+	static{
+		USER_SESSION = new HashSet<>();
+	}
+	
+	public static synchronized void addUser(User user){
+		USER_SESSION.add(user);
+	}
+	
+	public static synchronized void removeUser(User user){
+		USER_SESSION.remove(user);
+	}
+	
+	public static synchronized boolean containsUSer(User user) {
+		return USER_SESSION.contains(user);
+	}
+	
+	public static Set<User> getAllUser(){
+		return USER_SESSION;
+	}
+}
